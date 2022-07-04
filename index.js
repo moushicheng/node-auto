12f78adad7dacac6eae31c50f9233ff84587ced0
moushicheng <1163675107@qq.com>
2022-07-04 20:03:56 +0800

    Add image

--- index.js
+++ index.js
@@ -7,12 +7,8 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
-const {findImage} =require('./lib/index.js')
 const addon = require('./build/Release/second-native');
 const {find} =require('./lib/index.js')
-
-
-
 
 
 module.exports={
